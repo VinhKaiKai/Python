f262f947228c8b7bdd789ac8c74dd1fe2591f626
Vinh KaiKai <tramquangvinh12@gmail.com>
2024-02-19 15:41:19 +0700

    first commit

---
+++ bai1.py
@@ -0,0 +1,5 @@
+print("hello vinh moi hoc python")
+name = input("ni jiao shenme mingzi?")
+namsinh = int(input("ni sheng na ge nian?"))
+age = 2024 - namsinh
+print("ni hao " + name + "jin nian ni " + str(age) +"sui")
